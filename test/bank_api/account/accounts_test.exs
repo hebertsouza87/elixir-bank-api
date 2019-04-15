12d480bc377de36b27f5303ac45b1d82d1994289
Hebert Souza <hebertsouza87@gmail.com>
2019-04-15 19:01:04 -0300

    Add some more accounts test

--- test/bank_api/account/accounts_test.exs
+++ test/bank_api/account/accounts_test.exs
@@ -7,13 +7,23 @@ defmodule BankApi.AccountsTest do
   alias BankApi.Banking.TransactionQueries
 
   describe "accounts" do
-
     test "activate/1 activate an account" do
       account = insert(:account)
 
       {:ok, activated_account} = Accounts.activate(account.number)
       assert activated_account.status == "ACTIVE"
       assert TransactionQueries.get_amount_in_account(activated_account.id) == 100_000
+    end
+
+    test "report/3 do not get transaction out of range" do
+      today = get_today_as_string()
+      yesterday = Timex.now()
+                  |> Timex.shift(days: -1)
+
+      %{account: account} = insert(:transaction, inserted_at: yesterday)
+      {:ok, report} = Accounts.report(account, today, today)
+
+      assert [] == report
     end
 
     test "report/3 get today report" do
@@ -25,6 +35,24 @@ defmodule BankApi.AccountsTest do
       assert 1 == length(report)
     end
 
+    test "report/3 fail when end_date is greater than today" do
+      today = get_today_as_string()
+
+      %{account: account} = insert(:transaction)
+
+      assert Accounts.report(account, today, "3000-01-01") == {:error, {:end_date, "end_date is greater than today"}}
+    end
+
+    test "report/3 fail when start_date is greater than end_date" do
+      today = get_today_as_string()
+
+      %{account: account} = insert(:transaction)
+
+      assert Accounts.report(account, "3000-01-01", today) == {
+               :error,
+               {:start_date, "start_date is greater than end_date"}
+             }
+    end
   end
 
   defp get_today_as_string do
